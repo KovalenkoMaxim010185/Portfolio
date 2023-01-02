4e862f525edc8ce2c3e6d9de36985a6eb4776339
KovalenkoMaxim010185 <104447864+KovalenkoMaxim010185@users.noreply.github.com>
2023-01-03 01:45:07 +0200

    EN of BLOG

--- Desire/app/js/main.js
+++ Desire/app/js/main.js
@@ -20,6 +20,11 @@ $(function () {
         arrows: false,
     });
 
+    $('.article-slider__box').slick({
+        prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="../images/arrow-slide-left.svg"></button>',
+        nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="../images/arrow-slide-right.svg"></button>'
+    });
+
     var mixer = mixitup('.gallery__inner', {
         load: {
             filter: '.living'
