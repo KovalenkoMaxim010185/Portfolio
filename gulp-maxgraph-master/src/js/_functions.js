b148dc31023a875462e523a7bf155ebea99d8f1e
KovalenkoMaxim010185 <104447864+KovalenkoMaxim010185@users.noreply.github.com>
2023-02-12 17:03:11 +0200

    Repair of SLIDER is finished

--- gulp-maxgraph-master/src/js/_functions.js
+++ gulp-maxgraph-master/src/js/_functions.js
@@ -50,13 +50,24 @@ console.log(mobileCheck())
 // });
 
 // Подключение свайпера
-import Swiper, { Navigation, Pagination } from 'swiper';
-Swiper.use([Navigation, Pagination]);
-const swiper = new Swiper('.swiper', {
+const portSlider = document.querySelector('.portfolio-section__items');
+
+
+import Swiper, { Navigation } from 'swiper';
+Swiper.use([Navigation]);
+const portfolioSlider = new Swiper(portSlider, {
   slidesPerView: 3,
   spaceBetween: 30,
-  loop: true,
-
+  on: {
+    init: function () {
+      const activeSlide = portSlider.querySelector('.swiper-slide-active');
+      const nextActiveSlide = activeSlide.nextElementSibling;
+      const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
+      activeSlide.classList.add('slider-visible');
+      nextActiveSlide.classList.add('slider-visible');
+      nextNextActiveSlide.classList.add('slider-visible');
+    },
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.portfolio-section__next',
@@ -64,7 +75,29 @@ const swiper = new Swiper('.swiper', {
   },
 
 });
-
+document.querySelector('.portfolio-section__prev').addEventListener('click', () => {
+  const activeSlide = portSlider.querySelector('.swiper-slide-next');
+  document.querySelectorAll('.portfolio-section__items .swiper-slide').forEach(el => {
+    el.classList.remove('slider-visible');
+  });
+  if (activeSlide.previousElementSibling) {
+    const nextActiveSlide = activeSlide.previousElementSibling;
+    activeSlide.classList.add('slider-visible');
+    nextActiveSlide.classList.add('slider-visible');
+    activeSlide.nextElementSibling.classList.add('slider-visible');
+  }
+});
+  document.querySelector('.portfolio-section__next').addEventListener('click', () => {
+  const activeSlide = portSlider.querySelector('.swiper-slide-active');
+  const nextActiveSlide = activeSlide.nextElementSibling;
+  const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
+  document.querySelectorAll('.portfolio-section__items .swiper-slide').forEach(el => {
+    el.classList.remove('slider-visible');
+  });
+  activeSlide.classList.add('slider-visible');
+  nextActiveSlide.classList.add('slider-visible');
+  nextNextActiveSlide.classList.add('slider-visible');
+});
 // Подключение анимаций по скроллу
 // import AOS from 'aos';
 // AOS.init();
